7673aae2bea1adb0b8275a32382fee305d6985d6
ardasakarya <sakaryaarda@gmail.com>
2025-07-25 17:14:14 +0300

    anasayfa yönnlendirmeleri ve mobil uytumlu hale getirildi

--- anasayfa/script.js
+++ anasayfa/script.js
@@ -37,3 +37,23 @@
   
   });
 
+ const popupOverlay = document.getElementById("popup-overlay");
+const filterForm = document.getElementById("filter-form");
+const startButtons = document.getElementById("popup-start-buttons");
+
+function openPopup() {
+  popupOverlay.classList.remove("hidden");
+  document.body.style.overflow = "hidden"; // Arka plan kaymasını engelle
+}
+
+function showFilterForm() {
+  filterForm.classList.remove("hidden");
+  startButtons.classList.add("hidden");
+}
+
+function closePopup() {
+  popupOverlay.classList.add("hidden");
+  filterForm.classList.add("hidden");
+  startButtons.classList.remove("hidden");
+  document.body.style.overflow = ""; // Arka plan kaydırmayı tekrar aç
+}
